0ff01cbc12a637bb479a843230d55412b60b4784
TAG Enterprises LLC <35851336+gswiii@users.noreply.github.com>
2026-06-06 23:14:39 -0500

    Add files via upload

--- tag_holdem/flutter_bootstrap.js
+++ tag_holdem/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "451622890" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1988787201" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
